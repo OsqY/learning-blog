f80054f54943463f7728e41021e3a656cbaddda6
Oscar <oscarjosehpc@gmail.com>
2023-12-01 11:48:32 -0600

    Added tests for blogposts

--- test/models/blogpost_test.rb
+++ test/models/blogpost_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class BlogpostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blogposts" do
+    assert blogposts(:draft).draft?
+  end
+  test "draft? returns false for published blogposts" do
+    refute blogposts(:published).draft?
+  end
+  test "draft? returns false for scheduled blogposts" do
+    refute blogposts(:scheduled).draft?
+  end
+  test "published? returns true for published blogposts" do
+    assert blogposts(:published).published?
+  end
+  test "published? returns false for draft blogposts" do
+    refute blogposts(:draft).published?
+  end
+  test "published? returns false for scheduled blogposts" do
+    refute blogposts(:scheduled).published?
+  end
+  test "scheduled? returns false for published blogposts" do
+    refute blogposts(:published).scheduled?
+  end
+  test "scheduled? returns false for draft blogposts" do
+    refute blogposts(:draft).scheduled?
+  end
+  test "scheduled? returns true for scheduled blogposts" do
+    assert blogposts(:scheduled).scheduled?
+  end
+
 end
